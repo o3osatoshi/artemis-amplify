9535e3e0e3988c85461776b5121762ba38f5c3bd
Satoshi Ogura <117020745+o3osatoshi@users.noreply.github.com>
2023-03-07 22:59:02 +0900

    fix all

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -15,6 +15,14 @@ export type AmplifyDependentResourcesAttributes = {
       "Region": "string"
     }
   },
+  "hosting": {
+    "S3AndCloudFront": {
+      "HostingBucketName": "string",
+      "Region": "string",
+      "S3BucketSecureURL": "string",
+      "WebsiteURL": "string"
+    }
+  },
   "storage": {
     "artemis": {
       "Arn": "string",
--- interface/src/components/ApiCall.tsx
+++ interface/src/components/ApiCall.tsx
@@ -3,18 +3,15 @@ import ArtemisDataSource from "../infrastructure/artemisDataSource";
 
 export default class ApiCall extends React.Component<IProps, IState> {
   private readonly artemisDataSource = new ArtemisDataSource();
-
   constructor(props: IProps) {
     super(props);
     this.myFunc = this.myFunc.bind(this);
   }
-
   async myFunc() {
     console.log("try!");
     const data = await this.artemisDataSource.getP3Players();
     console.log(data);
   }
-
   render() {
     return (
       <div>
