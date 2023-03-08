90c3d714d556d10d470d50ccab1895b93412296e
Satoshi Ogura <117020745+o3osatoshi@users.noreply.github.com>
2023-03-08 10:44:27 +0900

    react work with local

--- src/infrastructure/artemisDataSource.ts
+++ src/infrastructure/artemisDataSource.ts
@@ -1,10 +1,12 @@
 import axios, { AxiosRequestConfig, AxiosResponse, Method } from "axios";
 
 export default class ArtemisDataSource {
-  baseURL: string;
-  apiKey: string;
+  private readonly env: string;
+  private readonly baseURL: string;
+  private readonly apiKey: string;
 
   constructor() {
+    this.env = process.env.REACT_APP_ENV || "";
     this.baseURL = process.env.REACT_APP_API_GATEWAY_BASE_URL || "";
     this.apiKey = process.env.REACT_APP_API_GATEWAY_KEY || "";
     if (!this.baseURL || !this.apiKey)
@@ -18,6 +20,14 @@ export default class ArtemisDataSource {
     data: object | null = null
   ): Promise<any> {
     Object.assign(headers, { "x-api-key": this.apiKey });
+    if (this.env === "local") {
+      const xApigatewayHeader = encodeURIComponent(JSON.stringify(data));
+      console.log(xApigatewayHeader);
+      Object.assign(headers, {
+        "x-apigateway-event": xApigatewayHeader,
+        "x-apigateway-context": xApigatewayHeader,
+      });
+    }
     const config: AxiosRequestConfig = {
       method: method,
       baseURL: this.baseURL,
@@ -31,7 +41,7 @@ export default class ArtemisDataSource {
 
   async getP3Players(): Promise<object> {
     console.log("getP3Players");
-    return this._call("GET", "/api/p3/players/0xde2").then(
+    return this._call("GET", "/api/p3/players/0xde3").then(
       (response: AxiosResponse) => {
         console.log(response.data);
         return response.data;
