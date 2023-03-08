99330d7ee047a67b3aaf4384224dfd009df587e0
Satoshi Ogura <117020745+o3osatoshi@users.noreply.github.com>
2023-03-08 09:09:35 +0900

    app.js to app.ts

--- amplify/backend/function/phase3/src/app.ts
+++ amplify/backend/function/phase3/src/app.ts
@@ -1,23 +1,13 @@
-/*
-Copyright 2017 - 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
-    http://aws.amazon.com/apache2.0/
-or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and limitations under the License.
-*/
-
-
-
-const AWS = require('aws-sdk')
-const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
-const bodyParser = require('body-parser')
-const express = require('express')
+import AWS from 'aws-sdk'
+import awsServerlessExpressMiddleware from 'aws-serverless-express/middleware'
+import bodyParser from 'body-parser'
+import express from 'express';
 
 AWS.config.update({ region: process.env.TABLE_REGION });
 
 
 let tableName = "p3Player";
-let dynamodb;
+let dynamodb: AWS.DynamoDB.DocumentClient;
 if (process.env.ENV && process.env.ENV !== "NONE") {
   tableName = tableName + '-' + process.env.ENV;
   dynamodb = new AWS.DynamoDB.DocumentClient({
@@ -31,7 +21,6 @@ const userIdPresent = false; // TODO: update in case is required to use that def
 const partitionKeyName = "walletAddress";
 const partitionKeyType = "S";
 const sortKeyName = "";
-const sortKeyType = "";
 const hasSortKey = sortKeyName !== "";
 const path = "/api/p3/players";
 const UNAUTH = 'UNAUTH';
@@ -51,7 +40,7 @@ app.use(function(req, res, next) {
 });
 
 // convert url string param to expected Type
-const convertUrlType = (param, type) => {
+const convertUrlType = (param: string, type: string) => {
   switch(type) {
     case "N":
       return Number.parseInt(param);
@@ -64,8 +53,12 @@ const convertUrlType = (param, type) => {
  * HTTP Get method for list objects *
  ********************************/
 
+interface Condition {
+  walletAddress: any
+}
+
 app.get(path + hashKeyPath, function(req, res) {
-  const condition = {}
+  const condition: Condition = {walletAddress: {}}
   condition[partitionKeyName] = {
     ComparisonOperator: 'EQ'
   }
@@ -86,6 +79,7 @@ app.get(path + hashKeyPath, function(req, res) {
     KeyConditions: condition
   }
 
+  // @ts-ignore
   dynamodb.query(queryParams, (err, data) => {
     if (err) {
       res.statusCode = 500;
@@ -100,22 +94,18 @@ app.get(path + hashKeyPath, function(req, res) {
  * HTTP Get method for get single object *
  *****************************************/
 
+interface Params {
+  walletAddress: any
+}
+
 app.get(path + '/object' + hashKeyPath + sortKeyPath, function(req, res) {
-  const params = {};
+  const params: Params = {walletAddress: {}};
   if (userIdPresent && req.apiGateway) {
     params[partitionKeyName] = req.apiGateway.event.requestContext.identity.cognitoIdentityId || UNAUTH;
   } else {
     params[partitionKeyName] = req.params[partitionKeyName];
     try {
       params[partitionKeyName] = convertUrlType(req.params[partitionKeyName], partitionKeyType);
-    } catch(err) {
-      res.statusCode = 500;
-      res.json({error: 'Wrong column type ' + err});
-    }
-  }
-  if (hasSortKey) {
-    try {
-      params[sortKeyName] = convertUrlType(req.params[sortKeyName], sortKeyType);
     } catch(err) {
       res.statusCode = 500;
       res.json({error: 'Wrong column type ' + err});
@@ -148,7 +138,7 @@ app.get(path + '/object' + hashKeyPath + sortKeyPath, function(req, res) {
 
 app.put(path, function(req, res) {
 
-  if (userIdPresent) {
+  if (userIdPresent && req.apiGateway){
     req.body['userId'] = req.apiGateway.event.requestContext.identity.cognitoIdentityId || UNAUTH;
   }
 
@@ -172,7 +162,7 @@ app.put(path, function(req, res) {
 
 app.post(path, function(req, res) {
 
-  if (userIdPresent) {
+  if (userIdPresent && req.apiGateway){
     req.body['userId'] = req.apiGateway.event.requestContext.identity.cognitoIdentityId || UNAUTH;
   }
 
@@ -195,21 +185,13 @@ app.post(path, function(req, res) {
 ***************************************/
 
 app.delete(path + '/object' + hashKeyPath + sortKeyPath, function(req, res) {
-  const params = {};
+  const params: Params = {walletAddress: {}};
   if (userIdPresent && req.apiGateway) {
     params[partitionKeyName] = req.apiGateway.event.requestContext.identity.cognitoIdentityId || UNAUTH;
   } else {
     params[partitionKeyName] = req.params[partitionKeyName];
      try {
       params[partitionKeyName] = convertUrlType(req.params[partitionKeyName], partitionKeyType);
-    } catch(err) {
-      res.statusCode = 500;
-      res.json({error: 'Wrong column type ' + err});
-    }
-  }
-  if (hasSortKey) {
-    try {
-      params[sortKeyName] = convertUrlType(req.params[sortKeyName], sortKeyType);
     } catch(err) {
       res.statusCode = 500;
       res.json({error: 'Wrong column type ' + err});
